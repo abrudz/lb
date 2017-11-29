e2d75c01068808f0cb04907c343f4712c524113b
abrudz <a.brudz@gmail.com>
2017-11-29 16:20:20 +0000

    add close button and ∆⍙

--- lb.js
+++ lb.js
@@ -1,7 +1,8 @@
 ;(_=>{
 let hc={'<':'&lt;','&':'&amp;',"'":'&apos;','"':'&quot;'},he=x=>x.replace(/[<&'"]/g,c=>hc[c]) //html chars and escape fn
 ,tcs='<-←-<←xx×:-÷-:÷*O⍟O*⍟[-⌹-]⌹OO○77⌈FF⌈ll⌊LL⌊|_⊥_|⊥TT⊤-|⊣|-⊢=/≠/=≠<=≤<_≤>=≥>_≥==≡=_≡/_≢_/≢vv∨^^∧^~⍲~^⍲v~⍱~v⍱^|↑|^↑v|↓|v↓((⊂(_⊆_(⊆))⊃[|⌷|]⌷A|⍋|A⍋V|⍒|V⍒ii⍳i_⍸_i⍸ee∊e_⍷_e⍷'+
-'uu∪UU∪nn∩/-⌿-/⌿\\-⍀-\\⍀,-⍪-,⍪rr⍴pp⍴O|⌽|O⌽O-⊖-O⊖O\\⍉\\O⍉::¨~:⍨:~⍨*:⍣:*⍣oo∘o:⍤:o⍤[\'⍞\']⍞[]⎕[:⍠:]⍠[=⌸=]⌸[<⌺>]⌺o_⍎_o⍎oT⍕To⍕<>⋄on⍝no⍝aa⍺ww⍵VV∇--¯0~⍬~0⍬[?⍰?]⍰:V⍢V:⍢||∥ox¤xo¤)_⊇_)⊇O:⍥:O⍥V~⍫~V⍫'
+'uu∪UU∪nn∩/-⌿-/⌿\\-⍀-\\⍀,-⍪-,⍪rr⍴pp⍴O|⌽|O⌽O-⊖-O⊖O\\⍉\\O⍉::¨~:⍨:~⍨*:⍣:*⍣oo∘o:⍤:o⍤[\'⍞\']⍞[]⎕[:⍠:]⍠[=⌸=]⌸[<⌺>]⌺o_⍎_o⍎oT⍕To⍕<>⋄on⍝no⍝aa⍺ww⍵VV∇--¯0~⍬~0⍬'+
+'^-∆-^∆^=⍙=^⍙[?⍰?]⍰:V⍢V:⍢||∥ox¤xo¤)_⊇_)⊇O:⍥:O⍥V~⍫~V⍫'
 ,lbs=['←ASSIGN',' ','+conjugate\nplus','-negate\nminus','×direction\ntimes','÷reciprocal\ndivide','*exponential\npower','⍟natural logarithm\nlogarithm',
 '⌹matrix inverse\nmatrix divide','○pi times\ncircular','!factorial\nbinomial','?roll\ndeal',' ','|magnitude\nresidue',
 '⌈ceiling\nmaximum','⌊floor\nminimum','⊥decode','⊤encode','⊣same\nleft','⊢same\nright',' ','=equal','≠not equal',
@@ -13,7 +14,7 @@ let hc={'<':'&lt;','&':'&amp;',"'":'&apos;','"':'&quot;'},he=x=>x.replace(/[<&'"
 '⍉transpose\nreorder axes',' ','¨Each','⍨Selfie\nSwap','⍣Repeat','.Outer Product (∘.)\nInner Product',
 '∘OUTER PRODUCT (∘.)\nCurry\nCompose','⍤Rank','@At',' ','⍞STDIN\nSTDERR','⎕EVALUATED STDIN\nSTDOUT','⍠Variant',
 '⌸Index Key\nKey\n','⌺Stencil','⌶I-Beam','⍎execute','⍕format',' ','⋄STATEMENT SEPARATOR','⍝COMMENT','→ABORT\nBRANCH','⍵RIGHT ARGUMENT','⍺LEFT ARGUMENT',
-'∇recursion','&Spawn',' ','¯NEGATIVE','⍬EMPTY NUMERIC VECTOR']
+'∇recursion','&Spawn',' ','¯NEGATIVE','⍬EMPTY NUMERIC VECTOR','∆IDENTIFIER CHARACTER','⍙IDENTIFIER CHARACTER']
 ,bqk=' =1234567890-qwertyuiop\\asdfghjk∙l;\'zxcvbnm,./`[]+!@#$%^&*()_QWERTYUIOP|ASDFGHJKL:"ZXCVBNM<>?~{}'.replace(/∙/g,'')
 ,bqv='`÷¨¯<≤=≥>≠∨∧×?⍵∊⍴~↑↓⍳○*⊢∙⍺⌈⌊_∇∆∘\'⎕⍎⍕∙⊂⊃∩∪⊥⊤|⍝⍀⌿⋄←→⌹⌶⍫⍒⍋⌽⍉⊖⍟⍱⍲!⍰W⍷⍷⍨YU⍸⍥⍣⊣ASDF⍢H⍤⌸⌷≡≢⊆⊇CVB¤∥⍪⍙⍠⌺⍞⍬'.replace(/∙/g,'')
 ,tc={},bqc={} //tab completions and ` completions
@@ -27,29 +28,25 @@ let lbh='';for(let i=0;i<lbs.length;i++){
   lbh+='<b title="'+he(lbs[i].slice(1))+(ks.length?'\n'+ks.join(''):'')+'">'+lbs[i][0]+'</b>'
 }
 let d=document,el=d.createElement('div');el.innerHTML=
-`<div class=ngn_lb>${lbh}</div>
+`<div class=ngn_lb><span class=ngn_x title=Close>❎</span>${lbh}</div>
+ <style>@font-face{font-family:"Apl385 Unicode";src:url(Apl385.woff)format('woff');}</style>
  <style>
-  @font-face{font-family:"Apl385 Unicode";src:url(https://abrudz.github.io/lb/Apl385.woff)format('woff');}
- </style>
- <style>
-  body{padding-top:24px!important}
-  .ngn_lb{position:fixed;top:0;left:0;right:0;padding:0 4px;line-height:24px;background-color:buttonface;color:#000;
-    cursor:default;font-family:"APL385 Unicode","DejaVu Sans Mono",monospace;
-    border-bottom:solid buttonshadow 1px;z-index:2147483647}
-  .ngn_lb b{cursor:pointer;padding:0 1px;font-weight:normal}
-  .ngn_lb b:hover,.ngn_bq .ngn_lb{background-color:buttonshadow;color:#fff}
+  .ngn_lb{position:fixed;top:0;left:0;right:0;margin:background-color:ButtonFace;color:ButtonText;cursor:default;z-index:2147483647;
+    font-family:"Apl385 Unicode",monospace;border-bottom:solid ButtonHighlight 1px;padding:0 4px;word-wrap:break-word}
+  .ngn_lb b{cursor:pointer;padding:0 1px;font-weight:normal;float:left}
+  .ngn_lb b:hover,.ngn_bq .ngn_lb{background-color:Highlight;color:HighlightText}
+  .ngn_x{float:right;color:ButtonShadow;cursor:pointer}
+  .ngn_x:hover{color:red}
  </style>`
 d.body.appendChild(el)
-let t,lb=el.firstChild,bqm=0 //t:textarea, lb:language bar, bqm:backquote mode
+let t,ts=[],lb=el.firstChild,bqm=0 //t:textarea or input, lb:language bar, bqm:backquote mode
 let pd=x=>x.preventDefault()
-lb.onmousedown=x=>{
-  if(x.target.nodeName!=='B'||!t)return
-  let i=t.selectionStart,j=t.selectionEnd,v=t.value,s=x.target.textContent
-  if(i!=null&&j!=null){t.value=v.slice(0,i)+s+v.slice(j);t.selectionStart=t.selectionEnd=i+s.length}
-  pd(x)
-}
-lb.onmousewheel=x=>{lb.scrollLeft-=x.wheelDelta/60}
-lb.addEventListener('DOMMouseScroll',x=>{pd(x);lb.onmousewheel({wheelDelta:x.detail*-120})}) //firefox
+let ev=(x,t,f,c)=>x.addEventListener(t,f,c)
+ev(lb,'mousedown',x=>{
+  if(x.target.classList.contains('ngn_x')){lb.hidden=1;upd();return!1}
+  if(x.target.nodeName==='B'&&!t){let i=t.selectionStart,j=t.selectionEnd,v=t.value,s=x.target.textContent
+    if(i!=null&&j!=null){t.value=v.slice(0,i)+s+v.slice(j);t.selectionStart=t.selectionEnd=i+s.length};pd(x);return!1}
+})
 let fk=x=>{
   let t=x.target
   if(bqm){let i=t.selectionStart,v=t.value,c=bqc[x.key];if(x.which>31){bqm=0;d.body.classList.remove('ngn_bq')}
@@ -63,7 +60,9 @@ let fk=x=>{
 let ff=x=>{
   let t0=x.target,nn=t0.nodeName.toLowerCase()
   if(nn!=='textarea'&&(nn!=='input'||t0.type!=='text'&&t0.type!=='search'))return
-  t=t0;if(!t.ngn){t.ngn=1;t.addEventListener('keydown',fk)}
+  t=t0;if(!t.ngn){t.ngn=1;ts.push(t);ev(t,'keydown',fk)}
 }
-d.addEventListener('focus',ff,!0);let ae=d.activeElement;ae&&ff({type:'focus',target:ae})
+let upd=_=>{d.body.style.marginTop=lb.clientHeight+'px'}
+upd();ev(window,'resize',upd)
+ev(d,'focus',ff,!0);let ae=d.activeElement;ae&&ff({type:'focus',target:ae})
 })();
