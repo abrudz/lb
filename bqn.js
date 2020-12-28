ce6651e826f466864e34c7da1dbdcd4c06563cee
razetime <rraghu.11502@gmail.com>
2020-12-28 20:51:23 +0530

    more adjustments from dzaima

--- bqn.js
+++ bqn.js
@@ -9,10 +9,11 @@
 	for (let i = 0; i < tcs.length; i += 3)tc[tcs[i] + tcs[i + 1]] = tcs[i + 2]
 	for (let i = 0; i < tcs.length; i += 3) { let k = tcs[i + 1] + tcs[i]; tc[k] = tc[k] || tcs[i + 2] }
 	let lbh = ''; for (let i = 0; i < lbs.length; i++) {
+		let [k, ...v] = [...lbs[i]]; v = v.join('');
 		let ks = []
-		for (let j = 0; j < tcs.length; j += 3)if (lbs[i][0] === tcs[j + 2]) ks.push('\n' + tcs[j] + ' ' + tcs[j + 1] + ' <tab>')
-		for (let j = 0; j < bqk.length; j++)if (lbs[i][0] === bqv[j]) ks.push('\n\\ ' + bqk[j])
-		lbh += '<b title="' + he(Array.from(lbs[i]).slice(1).join('') + (ks.length ? '\n' + ks.join('') : '')) + '">' + Array.from(lbs[i])[0] + '</b>'
+		for (let j = 0; j < tcs.length; j += 3)if (k === tcs[j + 2]) ks.push('\n' + tcs[j] + ' ' + tcs[j + 1] + ' <tab>')
+		for (let j = 0; j < bqk.length; j++)if (k === bqv[j]) ks.push('\n` ' + bqk[j])
+		lbh += '<b title="' + he(v + (ks.length ? '\n' + ks.join('') : '')) + '">' + k + '</b>'
 	}
 	let d = document, el = d.createElement('div'); el.innerHTML =
 		`<div class=ngn_lb><span class=ngn_x title=Close>❎</span>${lbh}</div>
@@ -43,7 +44,7 @@
 			let i = t.selectionStart, v = t.value, c = bqc[x.key]; if (x.which > 31) { bqm = 0; d.body.classList.remove('ngn_bq') }
 			if (c) { t.value = v.slice(0, i) + c + v.slice(i); t.selectionStart = t.selectionEnd = i + c.length; pd(x); return !1 }
 		}
-		if (!x.ctrlKey && !x.shiftKey && !x.altKey && !x.metaKey) {
+		else if (!x.ctrlKey && !x.shiftKey && !x.altKey && !x.metaKey) {
 			if ("\\½²^º§ùµ°".indexOf(x.key) > -1) {
 				bqm = 1; d.body.classList.add('ngn_bq'); pd(x); // ` or other trigger symbol pressed, wait for next key
 			} else if (x.key == "Tab") {
